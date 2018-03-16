cb1189e475f7d240cca1857e24b36bd544aaa1aa
soutolimaej <everton.soutolima@tno.nl>
2018-03-16 16:10:12 +0100

    changed function to work with deseq objects rather than file count files

--- R/deseq_transform.R
+++ R/deseq_transform.R
@@ -1,56 +1,15 @@
-transformDds <- function(dds, transformation) {
-  switch(transformation,
-         "raw", counts(dds),
-         "normalized", counts(dds, normalized = TRUE),
-         # Assumes you set the variance stabilized transformed counts to mcols(dds)$vst
-         "vst", mcols(dds)$vst,
-         stop("Unkown transformation"))
-}
-
-
-
 #' deseq_transform
 #'
-#' Perform deseq2 transformations such as rlog and vst.
+#' Perform deseq2 transformations such as rlog and vst. Returns a data.frame contaned the transformed counts.
 #'
+#' @param dds A DESeqDataSet object.
+#' @return A data.frame containing the transformed counts.
 #'
-deseq_transform <- function(countTable,sampData=NULL,
-                            deseq.design=~1,
+deseq_transform <- function(dds,
                             blind=TRUE,
-                            method='scale',
-                            out.path='../data/out/'){
+                            method='scale'){
+ require(DESeq2)
 
-  suppressWarnings(suppressMessages(library(DESeq2)))
-
-  deseq.design          <- as.formula(deseq.design)
-
-  # read data files
-  countTable.filename   <- sub("\\.[[:alnum:]]+$", "", basename(as.character(countTable)))
-  countTable            <- read.table(countTable,sep='\t',header=T,row.names=1,check.names=F,stringsAsFactors=F)
-
-  # add pseudocount
-  # Ideally, this function would perform a check before normalization and add pseucounts only when needed.
-  #countTable <- countTable+1
-
-  if(!is.null(sampData) | !blind){
-   sampData              <- read.table(sampData,sep='\t',header=T,row.names=1,check.names=F,stringsAsFactors=F)
-   sampData.rnames     <- row.names(sampData)
-
-   #sampData            <- data.frame(sampData[order(row.names(sampData)),])
-   #row.names(sampData) <- sampData.rnames
-
-
-   #countTable <- countTable[,order(names(countTable))]
-
-  } else {
-    sampData <- data.frame(data.frame(1:ncol(countTable)))
-    row.names(sampData) <- names(countTable)
-  }
-
-
-
-  # create DESeq2 obj
-  dds                   <- DESeqDataSetFromMatrix(countData = countTable,colData=sampData,design=deseq.design)
   dds                   <- estimateSizeFactors(dds)
   dds                   <- estimateDispersions(dds)
 
@@ -62,10 +21,6 @@ deseq_transform <- function(countTable,sampData=NULL,
                        stop('Unknown method'))
 
 
-  # write output
-  write.table(file = paste0(out.path,countTable.filename,'-','DESeq2_',gsub(toupper(method),pattern=' ',replacement='_'),ifelse(blind,'_BLIND',''),'.txt'),
-              x = countTable,
-              col.names=T,
-              row.names=T,sep='\t')
-}
 
+  return(countTable)
+}
